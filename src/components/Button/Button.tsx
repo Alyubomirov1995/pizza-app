525a986da43191d719372bdd71d0998eb3209483
Alyubomirov1995 <Profffundo@gmail.com>
2024-03-15 17:03:04 +0300

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import styles from './button.module.css';
+import { ButtonProps } from './Button.props';
+import cn from 'classnames';
+
+function Button({ children, className, appearence = 'small', ...props }: ButtonProps) {
+	return (
+		<button className={cn(styles['button'], styles['accent'], className, {
+			[styles['small']]: appearence ==='small',
+			[styles['big']]: appearence ==='big'
+		})} {...props}>{children}</button>
+	);
+}
+
+export default Button;
