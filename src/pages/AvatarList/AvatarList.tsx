d9bfb6348d97c24746318425a1f6ed50f0283e64
Alyubomirov1995 <Profffundo@gmail.com>
2024-04-11 14:21:14 +0300

    bugfix: Исправляет ошибку в листе аватаров

--- src/pages/AvatarList/AvatarList.tsx
+++ src/pages/AvatarList/AvatarList.tsx
@@ -1,4 +1,4 @@
-import  { useEffect } from 'react';
+import { useEffect } from 'react';
 import styles from './AvatarList.module.css';
 import Headling from '../../components/Headling/Headling';
 
@@ -7,15 +7,16 @@ export function AvatarList() {
 	useEffect(() => {
 		const savedAvatar = localStorage.getItem('selectedAvatar');
 		if (savedAvatar) {
-			document.getElementById('avatar').src = savedAvatar;
+			document.getElementById('avatar')!.src = savedAvatar;
+
 		}
 	}, []);
 
 	const handleAvatarClick = (event) => {
-		
+	
 		const selectedAvatar = event.target.src;
 		localStorage.setItem('selectedAvatar', selectedAvatar);
-		document.getElementById('avatar').src = selectedAvatar;
+		document.getElementById('avatar')!.src = selectedAvatar;
 	};
 	const avatarList = ['/Avatar.png', '/Avatar1.png', '/Avatar2.png', '/Avatar3.png', '/Avatar4.png', '/Avatar5.png'];
 
